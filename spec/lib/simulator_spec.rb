ed7ac7ff0eb93fa90169ecfea67f9c149eb841f6
Ardito Hartoto <dhartoto@gmail.com>
2015-07-22 16:40:58 +1000

    Add pending tests for Simulator#run

--- spec/lib/simulator_spec.rb
+++ spec/lib/simulator_spec.rb
@@ -8,4 +8,23 @@ describe Simulator do
       expect(simulator).to be_an_instance_of(Simulator)
     end
   end
+
+  describe '#run' do
+    it 'instruct user to enter a command.'
+    it 'requests user input'
+
+    context 'when user input valid command' do
+      it 'sends #receive to robot'
+      it 'displays response from robot'
+    end
+    context 'when user input invalid command' do
+      it 'does not send #receive to robot'
+      it 'displays error message'
+    end
+    context 'when user input EXIT' do
+      it 'does not send #receive to robot'
+      it 'displays exit message'
+      it 'exits the application'
+    end
+  end
 end

b25a0100bc7f5846f442b8650c04acc6b0ed3c4f
Ardito Hartoto <dhartoto@gmail.com>
2015-07-23 13:52:58 +1000

    Add #abort in Simulator#run
    Added abort with guard clause to run when user issues command to
    'EXIT'. Tests all passing with 1 pending.

--- lib/simulator.rb
+++ lib/simulator.rb
@@ -10,6 +10,7 @@ class Simulator
   def run
     puts display
     command = Command.build(gets.chomp)
+    abort("Exiting simulator...Goodbye!") if command == 'EXIT'
     resp = robot.receive(command) if command.valid?
     self.display = command.error_message || resp.message
     puts display

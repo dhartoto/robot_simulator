92406e89994e4e5338524dcf5c8d3e969bb2519b
Ardito Hartoto <dhartoto@gmail.com>
2015-07-23 12:10:32 +1000

    Add failing test to Command
    Add simple failing test for Command.build to complete building
    Simulator.
    'it responds to build' test failing.

--- spec/lib/command_spec.rb
+++ spec/lib/command_spec.rb
@@ -5,4 +5,7 @@ describe Command do
   it 'is a module' do
     expect(Command).to be_an_instance_of(Module)
   end
+  it 'responds to build' do
+    expect(Command).to respond_to(:build)
+  end
 end
